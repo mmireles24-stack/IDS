b3cb632c4d38490a2639bb9af929dd4f98aebce2
mmireles24-stack <mmireles_24@alu.uabcs.mx>
2026-02-20 10:50:04 -0700

    Prediseño de login

--- src/Main/Ventana.java
+++ src/Main/Ventana.java
@@ -2,8 +2,13 @@ package Main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Ventana extends JFrame{
 
@@ -17,6 +22,47 @@ public class Ventana extends JFrame{
 		this.setTitle("Holis");
 		this.setBackground(Color.BLACK);
 		this.setLocation(200,200);
+		this.setLayout(null);//Es para tener un marco, aqui no tenemos por el null
+		
+		JPanel contenedor = new JPanel();//seria nuestra hoja
+		contenedor.setOpaque(true);
+		contenedor.setBackground(Color.pink);
+		contenedor.setSize(500,500);
+		contenedor.setLocation(0,0);
+		contenedor.setLayout(null);
+		this.add(contenedor);
+		
+		
+		JLabel title_login = new JLabel();
+		title_login.setText("Bienvenido");
+		title_login.setSize(200,30);
+		title_login.setOpaque(true);
+		title_login.setLocation(150,10);
+		title_login.setBackground(Color.gray);
+		title_login.setFont(new Font("Arial",Font.BOLD,22));//Para darle fuente al texto
+		title_login.setHorizontalAlignment(JLabel.CENTER);
+		contenedor.add(title_login);
+		
+		JTextField username = new JTextField();
+		username.setSize(300,30);
+		username.setLocation(30,150);
+		username.setFont(new Font("Arial",Font.BOLD,22));
+		contenedor.add(username);
+		
+		JButton acceder= new JButton();
+		acceder.setText("Acceder");
+		acceder.setSize(180,50);
+		acceder.setLocation(150,350);
+		acceder.setFont(new Font("Arial",Font.BOLD,22));
+		contenedor.add(acceder);
+		
+		
+		contenedor.repaint();//refrescar la ventana una vez ejecutada 
+		contenedor.revalidate();
+		
+		
+		
+		
 		
 	}
 
